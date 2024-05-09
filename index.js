4297309a9d550aa57de651b8e31235637569a161
NaimoonJannat <naimoonjannat@gmail.com>
2024-05-10 00:58:28 +0600

    Deployed successfully

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 
 // middleware 
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173','http://b9a10-tourist-haven-server.vercel.app'],
   credentials: true
 }));
 app.use(express.json())
@@ -93,8 +93,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
